c205c83e92f57e74c389193b25fee6463ee48b09
innamagkeeva <inna.m79@mail.ru>
2025-03-20 02:37:38 +0300

    TODO на ts

--- main.ts
+++ main.ts
@@ -1,30 +1,30 @@
 export default{}
 
-type Operation = 'add'| 'multi' | 'subtract' | 'divide'
+// type Operation = 'add'| 'multi' | 'subtract' | 'divide'
 
-function calc(operation: Operation, a: number, b: number): number | string  {
-  switch (operation) {
-    case 'add':
-      return a + b
-    case 'subtract':
-          return a - b
-    case 'multi':
-        return a * b
-    case 'divide':
-      if (b === 0){
-      return 'делить на 0 нельзя'
-      }
-        return a / b 
-    default:
-      return 'выберите операцию'
-  }
+// function calc(operation: Operation, a: number, b: number): number | string  {
+//   switch (operation) {
+//     case 'add':
+//       return a + b
+//     case 'subtract':
+//           return a - b
+//     case 'multi':
+//         return a * b
+//     case 'divide':
+//       if (b === 0){
+//       return 'делить на 0 нельзя'
+//       }
+//         return a / b 
+//     default:
+//       return 'выберите операцию'
+//   }
   
-}
+// }
 
-console.log(calc('add', 5, 2));
-console.log(calc('multi', 5, 2));
-console.log(calc('divide', 5, 0));
-console.log(calc('divide', 10, 2));
+// console.log(calc('add', 5, 2));
+// console.log(calc('multi', 5, 2));
+// console.log(calc('divide', 5, 0));
+// console.log(calc('divide', 10, 2));
 
 
 
@@ -52,3 +52,34 @@ console.log(calc('divide', 10, 2));
 // console.log(calc('divide', 10, 2))
 
 
+
+
+
+
+const arr: Tasks[] = []
+
+type Tasks = 'купить хлеб' | 'почитать' | 'убрать' | 'покушать' 
+
+function addTask (task: Tasks) {
+  arr.push(task)
+  
+}
+
+addTask('почитать')
+addTask('покушать')
+addTask('убрать')
+ 
+console.log(arr);
+
+ function deleteTask (task: Tasks) {
+  
+  
+ }
+  
+ function setLocalStorage(kay: string, value: string): void {
+  localStorage.setItem(kay, value)
+}
+
+function removeLocalStorage (kay: string) {
+  localStorage.removeItem(kay)
+}
