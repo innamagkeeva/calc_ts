adf0e844285d9ed0d8d6c94d2dca7d479a6a541f
innamagkeeva <inna.m79@mail.ru>
2025-03-23 01:17:36 +0300

    изменила код и добавила функцию изменения статуса

--- main.ts
+++ main.ts
@@ -53,32 +53,27 @@ export default {}
 // console.log(calc('divide', 5, 0))
 // console.log(calc('divide', 10, 2))
 
-const arr: string[] = []
+type ObjTodo = {
+  text: string
+  status: string
+} // шаблон объекта
 
-function addTask(task: string) {
-  arr.push(task)
+const objTodo: ObjTodo[] = [] // массив с объектами задач  - текст и статус.
+
+type TaskStatus = 'в процессе' | 'сделать' | 'сделано' //  только такие статусы могут быть
+
+function addTask(task: string): void {
+  objTodo.push({ text: task, status: 'сделать' }) // статус 'сделать' будет по умолчанию пока не поменяется
 }
 
-addTask('почитать')
-addTask('покушать')
-addTask('убрать')
-
-// console.log('после добавления:', arr)
-
 function deleteTask(task: string): void {
-  const foundIndex = arr.findIndex((item) => item === task)
+  const foundIndex = objTodo.findIndex((item) => item.text === task) // findIndex ищет индекс задачи
   if (foundIndex === -1) {
     console.log('Задача не найдена')
     return
   }
-  arr.splice(foundIndex, 1)
+  objTodo.splice(foundIndex, 1) // удаляет задачу по найденному индексу
 }
-
-deleteTask('убрать')
-deleteTask('умыться')
-deleteTask('запустить салют')
-
-// console.log('после удаления:', arr)
 
 function setLocalStorage(kay: string, value: string): void {
   localStorage.setItem(kay, value)
@@ -88,8 +83,34 @@ function removeLocalStorage(kay: string): void {
   localStorage.removeItem(kay)
 }
 
-function showTasks(): void {
-  console.log(arr)
+function changeStatus(task: string, newStatus: TaskStatus): void {
+  const todoTask = objTodo.find((item) => item.text === task) // find ищет саму задачу по тексту, а не ее индекс
+  if (todoTask) {
+    todoTask.status = newStatus
+    console.log(
+      `Статус задачи "${todoTask.text}" изменен на "${todoTask.status}".`
+    )
+  } else {
+    console.log('такой задачи нет')
+  }
 }
+
+function showTasks(): void {
+  if (objTodo.length === 0) {
+    console.log('список задач пуст')
+    return
+  }
+  console.log(objTodo)
+}
+
+addTask('почитать')
+addTask('покушать')
+addTask('написать код')
+addTask('отдохнуть')
+deleteTask('почитать')
+deleteTask('погулять')
+
+changeStatus('покушать', 'в процессе')
+changeStatus('написать код', 'сделано')
 
 showTasks()
